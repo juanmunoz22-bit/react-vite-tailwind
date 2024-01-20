ac0fafe141f6ccb7b8b7b95c64fb5c79ddac50c2
Juan Munoz <juan.pablo982206@gmail.com>
2024-01-20 14:24:31 -0500

    create order card in cart checkout, add delete from cart function

--- src/Components/CheckoutSideMenu/index.jsx
+++ src/Components/CheckoutSideMenu/index.jsx
@@ -3,21 +3,41 @@ import { useContext } from "react"
 import { XMarkIcon } from "@heroicons/react/24/solid"
 
 import { ShoppingCartContext } from "../../Context"
+import OrderCard from "../OrderCard" 
 import "./styles.css"
 
 const CheckoutSideMenu = () => {
 
   const context = useContext(ShoppingCartContext)
 
+  const handleDelete = (id) => {
+    const filteredProducts = context.cartProducts.filter(product => product.id !== id)
+    context.setCartProducts(filteredProducts)
+  }
+
   return (
     <aside 
       className={`${context.isCheckoutMenuOpen ? 'flex' : 'hidden'}  checkout-side-menu flex-col fixed right-0 border border-black rounded-lg bg-white`}
     >
       <div className="flex justify-between items-center p-6">
-        <h2 className="font-medium text-xl">Cart</h2>
+        <h2 className="font-medium text-xl">My Cart</h2>
         <div onClick={() => context.closeCheckoutMenu()}>
           <XMarkIcon className="h-6 w-6 text-black cursor-pointer"/>
         </div>
+      </div>
+      <div className="px-6 overflow-y-auto">
+      {
+          context.cartProducts.map(product => (
+            <OrderCard
+              key={product.id}
+              id={product.id}
+              title={product.title}
+              image={product.images}
+              price={product.price}
+              handleDelete={handleDelete}
+            />
+          ))
+        }
       </div>
     </aside>
   )
